b90c74500c87fb747fad3a0d32a28ec6d68e62b4
dmitriy <dmitriy1a22222@gmail.com>
2018-09-10 19:41:46 +0300

    first makup

---
+++ test-assignment/src/component/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../stylesheets/styleForComponent/HeaderStyle.sass';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="Header">
+
+            </div>
+        );
+    }
+}
+
+export default Header;
